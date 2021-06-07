c86291f73dbeb7c0d2b603e1f39b9fd0b7c9fcd2
Nishant <nishantk03>
2021-06-08 00:51:36 +0530

    Show hardcoded time on loading screen

---
+++ lib/pages/loading.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  String time = 'loading';
+
+  void setWorldTime() async {
+    WorldTime worldTime = WorldTime(location: 'Kolkata', flag: 'india.png', url: 'Asia/Kolkata');
+    await worldTime.getTime();
+    setState(() {
+      time = worldTime.time;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
+    );
+  }
+}
---
+++ lib/services/world_time.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+
+class WorldTime {
+  String location;
+  String time;
+  String flag;
+  String url;
+  //Asia/Kolkata
+
+  WorldTime({ this.location, this.flag, this.url });
+
+  Future<void> getTime() async {
+    Response response = await get(Uri.parse("http://worldtimeapi.org/api/timezone/$url"));
+    Map timeMap = jsonDecode(response.body);
+    String dateTime = timeMap['utc_datetime'];
+    String offsetHrs = timeMap['utc_offset'].toString().substring(1,3);
+    String offsetMin = timeMap['utc_offset'].toString().substring(4,6);
+
+    DateTime now = DateTime.parse(dateTime);
+    now = now.add(Duration(hours: int.parse(offsetHrs)));
+    now = now.add(Duration(minutes: int.parse(offsetMin)));
+
+    time = now.toString();
+  }
+}
