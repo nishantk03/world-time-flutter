634eb999666e5102b7a0a67650d7c3eb9de8169c
Nishant <nishantk03>
2021-06-10 00:35:44 +0530

    error handling while calling time api

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -11,16 +11,21 @@ class WorldTime {
   WorldTime({ this.location, this.flag, this.url });
 
   Future<void> getTime() async {
-    Response response = await get(Uri.parse("http://worldtimeapi.org/api/timezone/$url"));
-    Map timeMap = jsonDecode(response.body);
-    String dateTime = timeMap['utc_datetime'];
-    String offsetHrs = timeMap['utc_offset'].toString().substring(1,3);
-    String offsetMin = timeMap['utc_offset'].toString().substring(4,6);
+    try{
+      Response response = await get(Uri.parse("http://worldtimeapi.org/api/timezone/$url"));
+      Map timeMap = jsonDecode(response.body);
+      String dateTime = timeMap['utc_datetime'];
+      String offsetHrs = timeMap['utc_offset'].toString().substring(1,3);
+      String offsetMin = timeMap['utc_offset'].toString().substring(4,6);
 
-    DateTime now = DateTime.parse(dateTime);
-    now = now.add(Duration(hours: int.parse(offsetHrs)));
-    now = now.add(Duration(minutes: int.parse(offsetMin)));
+      DateTime now = DateTime.parse(dateTime);
+      now = now.add(Duration(hours: int.parse(offsetHrs)));
+      now = now.add(Duration(minutes: int.parse(offsetMin)));
 
-    time = now.toString();
+      time = now.toString();
+    }catch(e){
+      print('Error caught $e ');
+      time = 'Error while loading time';
+    }
   }
 }
